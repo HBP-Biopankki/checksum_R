48912f13429fa07c9c6028059eea936a9c838c72
HBP-Biopankki <85553920+HBP-Biopankki@users.noreply.github.com>
2023-04-05 12:28:10 +0300

    Update checksum_R.r
    Fiksumpi rapa laskea seuraava täysi 10

--- checksum_R.r
+++ checksum_R.r
@@ -1,9 +1,6 @@
 
 Checksum_R <-function(my_str){
 
-  roundUp  <- function(x){round(x+5,-1)}
-
-
   magic_numbers <- c(7,3,1,7,3,1)
 
   palat  <- strsplit(my_str, "_")
@@ -14,7 +11,7 @@ Checksum_R <-function(my_str){
   master_list$mult = as.numeric(master_list[[1]]) * master_list[[2]]
   master_summa = sum(master_list$mult)
   
-  return (roundUp(master_summa) - master_summa)
+  return ((master_summa %/% 10 *10 +10) - master_summa)
 }
 
 Checksum_R('HBP_BB22_0027_323467')
